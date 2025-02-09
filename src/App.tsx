fae3840fde4a210526a3317a0e56521b151a921d
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-02-09 19:08:12 +0200

    feat: import nav component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import Nav from './components/nav';
+import { ThemeProvider } from './components/theme-provider';
+
 function App() {
   return (
-    <div className='flex flex-col items-center w-full h-screen max-w-7xl'>
-      <h1>Hello</h1>
-    </div>
+    <ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
+      <div className='w-full h-screen flex justify-center bg-slate-50 dark:bg-slate-950 text-slate-950 dark:text-slate-50 bg-[url(/noise.svg)]'>
+        <div className='max-w-7xl w-full flex flex-col items-center'>
+          <Nav />
+          <h1>Hello</h1>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
