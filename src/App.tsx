e87df5872b3d6c14c5dca09e3ef568ee3b3e45c3
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-02-11 19:28:11 +0200

    feat: import input and insight components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,47 @@
-import Nav from './components/nav';
-import { ThemeProvider } from './components/theme-provider';
+import { ChangeEvent, useState } from "react";
+import Input from "./components/input";
+import Insights from "./components/insights";
+import Nav from "./components/nav";
+import { ThemeProvider } from "./components/theme-provider";
 
 function App() {
+  const [text, setText] = useState("");
+  const [includeSpace, setIncludeSpace] = useState(false);
+  const [characterLimit, setCharacterLimit] = useState(false);
+
+  const handleText = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value);
+  };
+
+  const handleIncludeSpaceChange = () => {
+    setIncludeSpace((prev) => !prev);
+  };
+
+  const handleCharacterLimitChange = () => {
+    setCharacterLimit((prev) => !prev);
+  };
+
   return (
-    <ThemeProvider defaultTheme='light' storageKey='vite-ui-theme'>
-      <div className='w-full h-screen flex justify-center bg-slate-50 dark:bg-slate-950 text-slate-950 dark:text-slate-50 bg-[url(/noise.svg)]'>
-        <div className='max-w-7xl w-full flex flex-col items-center'>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <div className="flex min-h-screen w-full justify-center bg-slate-50 bg-[url(/noise.svg)] text-slate-950 dark:bg-slate-950 dark:text-slate-50">
+        <div className="flex w-full max-w-7xl flex-col items-center gap-8 p-4">
           <Nav />
-          <h1>Hello</h1>
+          <h1 className="text-center text-4xl font-bold">
+            Analyze your text in real-time.
+          </h1>
+          <Input
+            text={text}
+            handleText={handleText}
+            includeSpace={includeSpace}
+            handleIncludeSpaceChange={handleIncludeSpaceChange}
+            characterLimit={characterLimit}
+            handleCharacterLimitChange={handleCharacterLimitChange}
+          />
+          <Insights
+            text={text}
+            includeSpace={includeSpace}
+            characterLimit={characterLimit}
+          />
         </div>
       </div>
     </ThemeProvider>
