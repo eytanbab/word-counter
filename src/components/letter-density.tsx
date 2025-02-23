6bc5f17dfbbd3b58831196675253e6b58f78cccf
eytanbab <73233191+eytanbab@users.noreply.github.com>
2025-02-23 16:00:46 +0200

    feat: add load more letters density button

--- src/components/letter-density.tsx
+++ src/components/letter-density.tsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
+
 type Props = {
   charFreq: Record<string, number>;
   charactersCount: number;
 };
 
 const LetterDensity = ({ charFreq, charactersCount }: Props) => {
+  const [maxLetters, setMaxLetters] = useState(5);
+
   if (!charactersCount) {
     return (
       <p className="font-light text-slate-600 dark:text-slate-400">
@@ -16,10 +20,14 @@ const LetterDensity = ({ charFreq, charactersCount }: Props) => {
 
   const maxWidth = letterDensity[0][1].toString().length + 7;
 
+  const handleLoadMore = () => {
+    setMaxLetters((prev) => prev + 5);
+  };
+
   return (
     <div className="w-full">
       <h1 className="text-lg font-bold">Letter Density</h1>
-      {letterDensity.map(([char, count]) => (
+      {letterDensity.slice(0, maxLetters).map(([char, count]) => (
         <div className="flex w-full items-center gap-2">
           {/* Letter */}
           <span className="w-[2ch] text-center">{char}</span>
@@ -35,10 +43,15 @@ const LetterDensity = ({ charFreq, charactersCount }: Props) => {
             style={{ width: maxWidth + "ch" }}
             className="shrink-0 text-right"
           >
-            {count} ({Math.round((count / charactersCount) * 100) / 100}%)
+            {count} ({Math.round((count / charactersCount) * 100)}%)
           </span>
         </div>
       ))}
+      {charactersCount && maxLetters <= letterDensity.length && (
+        <button className="mt-2" onClick={handleLoadMore}>
+          Load more
+        </button>
+      )}
     </div>
   );
 };
